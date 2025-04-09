bda457df22cf2feb144b231b188fd2a58e8efac5
suzcube <avasuzannekerns@gmail.com>
2025-04-09 16:57:54 -0400

    added functions

--- calculator.py
+++ calculator.py
@@ -5,7 +5,28 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+
+def log(a, b): loga(b)# use math library + raise ValueError
+
+def exp(a, b): ab
+
+
+#lfckgk
 
 
