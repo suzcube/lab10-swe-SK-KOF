470f42cf5fc244b5b2633512e3b83622465a1530
newmanhw <newmanhwaters@gmail.com>
2025-04-09 00:59:32 -0400

    modified assignment

--- test_calculator.py
+++ test_calculator.py
@@ -3,42 +3,53 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     # Partner 2
-    def test_add(self):
+    def test_add(self): # 3 assertions
         pass
 
-    def test_subtract(self):
+    def test_subtract(self): # 3 assertions
         pass
     ##########################
 
     # Partner 1
-    def test_multiply(self):
+    def test_multiply(self): # 3 assertions
         pass
 
-    def test_divide(self):
+    def test_divide(self): # 3 assertions
         pass
     ##########################
 
     # Partner 2
-    def test_divide_by_zero(self):
-        # call division function inside
-        with self.assertRaises(ValueError):
-            pass
+    def test_divide_by_zero(self): # 1 assertion
+        # call division function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     div(0, 5)
+        pass
 
-    def test_logarithm(self):
+    def test_logarithm(self): # 3 assertions
+        pass
+
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero
         pass
     ##########################
     
     # Partner 1
-    def test_hypotenuse(self):
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
         pass
 
-    def test_hypotenuse_negative_sides(self):
-        # Test for invalid side lengths
-        with self.assertRaises(ValueError):
-            pass
+    def test_hypotenuse(self): # 3 assertions
+        pass
+
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        # Test basic function
+        pass
     ##########################
-    
-    # Can add more tests if you want
 
 # Do not touch this
 if __name__ == "__main__":
