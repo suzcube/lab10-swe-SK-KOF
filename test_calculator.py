a5ab4fc5ed265c22f0d8e9f13b83124730d1cb2f
suzcube <avasuzannekerns@gmail.com>
2025-04-13 00:49:40 -0400

    added functions

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,10 @@
-import unittest
-
-import calculator
-from calculator import *
 #https://github.com/suzcube/lab10-swe-SK-KOF.git
 # Partner 1: Katie Flanagan
 # Partner 2: Suzanne Kerns
+import unittest
+import calculator
+from calculator import *
+
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -25,9 +25,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(mul(0, 100), 0)
 
     def test_divide(self):
-        self.assertEqual(div(10, 2), 5)
-        self.assertEqual(div(-9, 3), -3)
-        self.assertAlmostEqual(div(1, 3), 0.3333, places=4)
+        self.assertEqual(div(2, 10), 5)
+        self.assertEqual(div(3, -9), -3)
+        self.assertAlmostEqual(div(3, 1), 0.3333, places=4)
 
 
     ######## Partner 2
@@ -39,28 +39,30 @@ class TestCalculator(unittest.TestCase):
 
     #     fill in code
 
-    def test_logarithm(self): # 3 assertions
-        self.assertEqual()
+    def test_logarithm(self):
+        self.assertEqual(logarithm(10, 1), 1)
+        self.assertEqual(logarithm(4, 2), 2)
+        self.assertNotEqual(logarithm(20, 100), 0)
 
     def test_log_invalid_base(self):
         with self.assertRaises(ZeroDivisionError):
-            self.assertNotEqual(calculator.logarithm(0, 0), 0)
+            self.assertNotEqual(calculator.logarithm(10, 10), 10)
     
     ######## Partner 1
     def test_log_invalid_argument(self):  # 1 assertion
         with self.assertRaises(ValueError):
-            self.assertNotEqual(calculator.logarithm((-10, 10), 10))
+            self.assertNotEqual(calculator.logarithm((10, 10), 10))
 
-    def test_hypotenuse(self): # 3 assertions
-        self.assertEqual(calculator.hypotenuse(10, 10), 12)
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(5, 12), 13)
         self.assertEqual(calculator.hypotenuse(3, 4), 5)
         self.assertEqual(calculator.hypotenuse(32, 41), 5)
 
-    def test_sqrt(self): # 3 assertions
+    def test_sqrt(self):
         with self.assertRaises(ValueError):
-            self.assertNotEqual(calculator.div(0, 5), -1)
-            self.assertNotEqual(calculator.div(0, 4), -1)
-            self.assertNotEqual(calculator.div(0, 0), 0)
+            self.assertEqual(calculator.square_root(16), 4)
+            self.assertEqual(calculator.square_root(1), 1)
+            self.assertNotEqual(calculator.square_root(64), 8)
 
 
 
